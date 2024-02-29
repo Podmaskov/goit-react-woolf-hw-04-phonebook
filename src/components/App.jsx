8cc734263d480a10671a7f86dfda21b58ec65d87
Podmaskov <podmaskov@gmail.com>
2024-02-29 11:38:40 +0100

    hw-04-phonebook

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,79 @@
+import React, { useState, useEffect } from 'react';
+import { ContactForm } from './ContactForm/ContactForm';
+import { Filter } from './Filter/Filter';
+import { ContactList } from './ContactList/ContactList';
+import { nanoid } from 'nanoid';
+import { setLocalStorage, getLocalStorage } from '../helpers/localStorage';
+
+import styles from './styles.module.css';
+
+const LS_KEY = 'contacts';
 export const App = () => {
+  const [contacts, setContacts] = useState([]);
+  const [filter, setFilter] = useState('');
+
+  useEffect(() => {
+    const contacts = getLocalStorage(LS_KEY);
+    if (contacts && contacts.length > 0) {
+      setContacts(contacts);
+    }
+  }, [setContacts]);
+
+  useEffect(() => {
+    setLocalStorage(LS_KEY, contacts);
+  }, [contacts]);
+
+  const handleFilterChange = event => {
+    const { value } = event.target;
+    setFilter(value);
+  };
+
+  const checkNameDuplicate = name => {
+    return contacts.some(
+      contact => contact.name.toLowerCase() === name.toLowerCase()
+    );
+  };
+
+  const addContact = ({ name, number }) => {
+    if (checkNameDuplicate(name)) {
+      alert(`${name} is already in contacts`);
+      return;
+    }
+    setContacts(prevState => {
+      return [
+        {
+          id: nanoid(),
+          name,
+          number,
+        },
+        ...prevState,
+      ];
+    });
+  };
+
+  const deleteContact = id => {
+    setContacts(prevState => {
+      return prevState.filter(contact => contact.id !== id);
+    });
+  };
+
+  const filteredContacts = () => {
+    const normalizedFilter = filter.toLowerCase();
+    return contacts.filter(contact =>
+      contact.name.toLowerCase().includes(normalizedFilter)
+    );
+  };
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div className={styles['main-container']}>
+      <h1>Phonebook</h1>
+      <ContactForm onSubmit={addContact} />
+      <h2>Contacts</h2>
+      <Filter filter={filter} onChange={handleFilterChange} />
+      <ContactList
+        contacts={filteredContacts()}
+        deleteContact={deleteContact}
+      />
     </div>
   );
 };
